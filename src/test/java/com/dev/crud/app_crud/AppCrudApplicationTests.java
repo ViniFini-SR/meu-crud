2d852bcd5abc6b833b9608c41d61a3b52adbfca0
ViniFini-SR <viniciossr12@gmail.com>
2026-01-14 21:24:06 +0000

    Iniciando projeto, ambiente dev container, spring e dependencias com mvnw

---
+++ src/test/java/com/dev/crud/app_crud/AppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.crud.app_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
